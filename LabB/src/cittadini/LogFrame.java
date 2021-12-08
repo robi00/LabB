92770e7f4b2e17a665d0344aebe612f7ed12f0e2
Lorenzo <lorenzo.meregalli.work@gmail.com>
2021-12-08 17:01:03 +0100

    Interfaccia centri vaccinali (con problema su layout)

--- LabB/src/cittadini/LogFrame.java
+++ LabB/src/cittadini/LogFrame.java
@@ -1,29 +1,36 @@
 package cittadini;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class LogFrame extends JPanel {
 
 	// Crea pannello
-	
+
 	public LogFrame() {
 		initComponents2();
 		createEvent2();
 	}
-	
+
 	// Questo metodo contiene e inizializza i componenti
-	
-	private void initComponents2()
-	{
+
+	private void initComponents2() {
+
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
-		
+
 		JLabel lblNewLabel = new JLabel("REGISTRAZIONE EFFETTUATA");
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
@@ -31,7 +38,7 @@ public class LogFrame extends JPanel {
 		gbc_lblNewLabel.gridx = 0;
 		gbc_lblNewLabel.gridy = 0;
 		add(lblNewLabel, gbc_lblNewLabel);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("CON SUCCESSO");
 		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
@@ -39,14 +46,14 @@ public class LogFrame extends JPanel {
 		gbc_lblNewLabel_1.gridx = 0;
 		gbc_lblNewLabel_1.gridy = 1;
 		add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
+
 		JButton btnNewButton = new JButton("CERCA CENTRO");
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.insets = new Insets(0, 100, 10, 0);
 		gbc_btnNewButton.gridx = 0;
 		gbc_btnNewButton.gridy = 2;
 		add(btnNewButton, gbc_btnNewButton);
-		
+
 		JButton btnNewButton_1 = new JButton("TORNA HOME");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -63,11 +70,11 @@ public class LogFrame extends JPanel {
 		gbc_btnNewButton_1.gridy = 3;
 		add(btnNewButton_1, gbc_btnNewButton_1);
 	}
-	
+
 	// Questo metodo contiene gli eventi
-	
-	private void createEvent2() 
-	{
+
+	private void createEvent2() {
+
 		/* da riempire */
 	}
 
--- LabB/src/cittadini/cittadiMain.java
+++ LabB/src/cittadini/cittadiMain.java
@@ -1,35 +1,33 @@
 package cittadini;
 
+import java.awt.EventQueue;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JFrame;
 
-public class cittadiMain extends JFrame
-{
+public class cittadiMain extends JFrame {
+
 	// Crea frame
-	public static void main(String[] args) 
-	{
-			
-			EventQueue.invokeLater(new Runnable() 
-			{
-				public void run() 
-				{
-					try 
-					{
-						JFrame frame = new JFrame();
-						cittaFrame panel = new cittaFrame();
-		                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		                frame.setBounds(100, 100, 500, 600);
-		                frame.setVisible(true);
-		                frame.setTitle("Piattaforma vaccinale");
-		                frame.getContentPane().add(panel);
-		     
-					} catch (Exception e) 
-					{
-						e.printStackTrace();
-					}
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+
+				try {
+
+					JFrame frame = new JFrame();
+					cittaFrame panel = new cittaFrame();
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					frame.setBounds(100, 100, 500, 600);
+					frame.setVisible(true);
+					frame.setTitle("Piattaforma vaccinale");
+					frame.getContentPane().add(panel);
+
+				} catch (Exception e) {
+
+					e.printStackTrace();
 				}
-			});
-		}
+			}
+		});
+	}
 }
